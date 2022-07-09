d0d35b645cff9e2aa3c2c1403bee385a503107cc
Rafael Arantes <97065934+365rafael@users.noreply.github.com>
2022-07-09 11:58:52 -0300

    Update firebaseConnection.js

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -3,21 +3,7 @@ import "firebase/auth";
 import "firebase/database";
 
 let firebaseConfig = {
-  apiKey: "AIzaSyCPca_EJMf460r1p-yhc8Ww542aCT0CZms",
-
-  authDomain: "meuapp-b1a00.firebaseapp.com",
-
-  databaseURL: "https://meuapp-b1a00-default-rtdb.firebaseio.com",
-
-  projectId: "meuapp-b1a00",
-
-  storageBucket: "meuapp-b1a00.appspot.com",
-
-  messagingSenderId: "811682907035",
-
-  appId: "1:811682907035:web:2ca1f3cacaa42c0a5e973e",
-
-  measurementId: "G-XJ42JKP4RD",
+  //aqui você insere os valores obtidos em seu firebase
 };
 
 if (!firebase.apps.length) {
